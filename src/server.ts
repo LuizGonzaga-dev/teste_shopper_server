6cd2fb003d6e9f79a1f3a1d84d73188b1a39143f
LuizGonzaga <75226617+LuizGonzaga-dev@users.noreply.github.com>
2024-08-27 10:19:37 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import http from 'http';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const runServer = (port: number, server: http.Server) => {
+    server.listen(port, () => {
+        console.log(`http://localhost:${port}`);
+    })
+};
+
+const regularServer = http.createServer(app);
+const serverPort: number = process.env.PORT ? parseInt(process.env.PORT) : 9000;
+runServer(serverPort, regularServer);
+
