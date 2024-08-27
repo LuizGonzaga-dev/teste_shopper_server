5ed259054cfbfc10a2534c5d2227cb1fb7e8ec57
LuizGonzaga <75226617+LuizGonzaga-dev@users.noreply.github.com>
2024-08-27 10:45:17 -0300

    configuração arquivo de rotas

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,17 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import http from 'http';
+import routes from './routes/routes';
+import {requestIntercepter} from './utils/requestIntercepter';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.all("*", requestIntercepter);
+app.use("/", routes);
 
 const runServer = (port: number, server: http.Server) => {
     server.listen(port, () => {
